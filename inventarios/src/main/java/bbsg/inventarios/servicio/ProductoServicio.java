44295cb59a146837216aad06a95208979e6b4268
Samuel <samuelariaspenalo.com>
2025-01-10 07:48:14 -0400

    Agregue los servicios de la funciones

--- inventarios/src/main/java/bbsg/inventarios/servicio/ProductoServicio.java
+++ inventarios/src/main/java/bbsg/inventarios/servicio/ProductoServicio.java
@@ -1,27 +1,37 @@
 package bbsg.inventarios.servicio;
 
 import bbsg.inventarios.modelo.Producto;
+import bbsg.inventarios.repositorio.ProductoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductoServicio implements IProductoServicio{
+
+    @Autowired
+    private ProductoRepositorio productoRepositorio;
+
     @Override
     public List<Producto> listarProductos() {
-        return List.of();
+        return  this.productoRepositorio.findAll();
     }
 
     @Override
     public Producto buscarProductoPorId(Integer idProducto) {
-        return null;
+        Producto producto = this.productoRepositorio.findById(idProducto).orElse(null);
+        return producto;
     }
 
     @Override
     public void guardarProducto(Producto producto) {
+        this.productoRepositorio.save(producto);
 
     }
 
     @Override
     public void eliminarProductoPorId(Integer idProducto) {
-
+            this.productoRepositorio.deleteById(idProducto);
     }
 }
