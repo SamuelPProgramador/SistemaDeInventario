9279a782ecdd67d19b59896210ec79ae3aa46013
Samuel <samuelariaspenalo.com>
2025-01-10 08:10:07 -0400

    Agregue controlador rest

---
+++ inventarios/src/main/java/bbsg/inventarios/controlador/productoControlador.java
@@ -0,0 +1,34 @@
+package bbsg.inventarios.controlador;
+
+import bbsg.inventarios.modelo.Producto;
+import bbsg.inventarios.servicio.ProductoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//http://localhost:8080/inventario-app
+@RequestMapping("inventario-app")
+@CrossOrigin(value = "http://localhost:4200")
+public class productoControlador {
+    private static  final Logger logger =
+            LoggerFactory.getLogger(productoControlador.class);
+
+    @Autowired
+    private ProductoServicio productoServicio;
+
+    //htpp://localhost:8080/inventario-app/producto
+    @GetMapping("/productos")
+    public List<Producto> obtenerProducto(){
+        List<Producto> productos = this.productoServicio.listarProductos();
+        logger.info("Productos Obtenido:");
+        productos.forEach((producto -> logger.info(producto.toString())));
+        return productos;
+    }
+}
